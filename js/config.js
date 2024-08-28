816c3985339676ccb4b485695e8ec7b7614a2dc8
su8 <wifiextender2@gmail.com>
2024-08-28 15:16:08 +0300

    new post about blk

--- js/config.js
+++ js/config.js
@@ -166,7 +166,8 @@
 ["urxvt_man2pdf","Second urxvt extensions revisited","August 27,2024",""],
 ["expanding_luks","Expanding encrypted container","August 27,2024",""],
 ["pylogin","Login module for your app","August 28,2024",""],
-["pacman_log_search","Pacman log searching","August 28,2024",""]
+["pacman_log_search","Pacman log searching","August 28,2024",""],
+["blk","Backup like king","August 28,2024",""]
 
   ];
 
